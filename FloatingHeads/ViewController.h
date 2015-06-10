54df5c3d0ad9c3616b8df1cf169c4663134acb7b
Alain K <alain.kahwaji@gmail.com>
2015-06-10 14:20:22 -0700

    Add two imageViews, associate them with actual images, adjust layers of imageViews in Storyboard

---
+++ FloatingHeads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FloatingHeads
+//
+//  Created by Alain  on 2015-06-10.
+//  Copyright (c) 2015 Alain . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
